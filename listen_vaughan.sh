310f1ce9ee9bf5740dd297d62cd581b5a239d217
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2016-09-15 14:15:26 +0200

    listen_vaughan: Improved script.

--- listen_vaughan.sh
+++ listen_vaughan.sh
@@ -1,5 +1,38 @@
 #! /bin/bash
 
-VAUGHAN_RADIO_STREAM="http://vaughanradioweb.streaming-pro.com:8057/;streamdemo_mp3.nsv"
+function start_listen 
+{
+	VAUGHAN_RADIO_STREAM="http://vaughanradioweb.streaming-pro.com:8057/;streamdemo_mp3.nsv"
+	mplayer ${VAUGHAN_RADIO_STREAM}
+}
 
-mplayer ${VAUGHAN_RADIO_STREAM}
+function stop_listen
+{
+	killall mplayer
+}
+
+function restart_listen
+{
+	stop_listen
+	start_listen
+}
+
+MODE="$1"
+
+case ${MODE} in
+"start")
+	start_listen
+	;;
+"stop")
+	stop_listen
+	;;
+"restart")
+	restart_listen
+	;;
+*)
+	VAUGHAN_SCRIPT_NAME=$(basename $0)
+	echo "${VAUGHAN_SCRIPT_NAME} <start>|<stop>|<restart>"
+	;;
+esac
+
+
